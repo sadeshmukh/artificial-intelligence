548cdeeadc75f61fcb7a9ffde7957bf7a0c7856a
Sahil Deshmukh <thesahildeshmukh@gmail.com>
2023-05-06 20:08:58 -0700

    Initial commit

---
+++ server.js
@@ -0,0 +1,13 @@
+express = require("express");
+app = express();
+app.use(express.static(__dirname + "/public"));
+
+app.get("/", function (req, res) {
+  res.sendFile(`${__dirname}/index.html`);
+});
+
+app.listen(process.env.PORT || 3000, function () {
+  console.log(
+    `Server running at http://localhost:${process.env.PORT || 3000}/`
+  );
+});
