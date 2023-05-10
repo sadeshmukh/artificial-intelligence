f84c023153889a3904adfb3a94194e890de764b6
Sahil Deshmukh <thesahildeshmukh@gmail.com>
2023-05-09 21:10:23 -0700

    Import/export + reset context + localStorage context

--- public/js/index.js
+++ public/js/index.js
@@ -1,29 +1,86 @@
 const chatSubmitButton = document.getElementById("chatSubmit");
 const chatInput = document.getElementById("chatInput");
 const outputDiv = document.getElementById("outputDiv");
+const settingsModal = new bootstrap.Modal(
+  document.getElementById("settingsModal")
+);
+const exportText = document.getElementById("exportText");
+const exportModal = new bootstrap.Modal(document.getElementById("exportModal"));
+
+const encryptionKey = "12345";
 
 const userInput = document.getElementById("userInput");
 const output = document.getElementById("output");
 
-const globalContext = [];
+let globalContext = JSON.parse(localStorage.getItem("globalContext"));
+if (!globalContext) {
+  localStorage.setItem("globalContext", JSON.stringify([]));
+  globalContext = [];
+}
+renderOutput();
+
 let contextLength = 3;
 let globalSystemMessage = "You are an affable, friendly chatbot";
 
-// async function get_api() {
-//   return fetch("/api/upper", {
-//     method: "POST",
-//     headers: {
-//       Accept: "application/json",
-//       "Content-Type": "application/json",
-//     },
-//     body: JSON.stringify({ text: "sometext" }),
-//   })
-//     .then((response) => response.json())
-//     .then((response) => response.response);
-// }
-// get_api().then((response) => alert(response));
+const resetGlobalContext = (event = null) => {
+  if (!event) {
+    globalContext = [];
+    localStorage.setItem("globalContext", JSON.stringify(globalContext));
+    return;
+  }
+  event.preventDefault();
+  globalContext = [];
+  localStorage.setItem("globalContext", JSON.stringify(globalContext));
+  renderOutput();
+  console.log(Object.keys(settingsModal));
+  settingsModal.hide();
+};
 
-async function get_ai_api(context, system) {
+const addGlobalContext = (context) => {
+  globalContext.push(context);
+  localStorage.setItem("globalContext", JSON.stringify(globalContext));
+  renderOutput();
+};
+
+const updateContextLength = (length) => {
+  contextLength = length;
+};
+
+const openExportModal = (event) => {
+  event.preventDefault();
+  exportContext();
+  exportModal.show();
+};
+
+const exportContext = () => {
+  const encryptedContext = CryptoJS.AES.encrypt(
+    JSON.stringify(globalContext),
+    encryptionKey
+  ).toString();
+  exportText.value = encryptedContext;
+  console.log(exportText.value);
+};
+
+const importContext = () => {
+  const encryptedContext = exportText.value;
+  try {
+    const decryptedContext = CryptoJS.AES.decrypt(
+      encryptedContext,
+      encryptionKey
+    ).toString(CryptoJS.enc.Utf8);
+    const parsedContext = JSON.parse(decryptedContext);
+    if (parsedContext) {
+      globalContext = parsedContext;
+      localStorage.setItem("globalContext", JSON.stringify(globalContext));
+      renderOutput();
+      exportModal.hide();
+    }
+  } catch (error) {
+    alert("Invalid context");
+  }
+};
+
+const get_ai_api = async function (context, system) {
   if (contextLength === 0) {
     // .slice(0) returns a copy of the array
     // Goal is to at least have user's context
@@ -43,7 +100,7 @@ async function get_ai_api(context, system) {
   })
     .then((response) => response.json())
     .then((response) => response.output);
-}
+};
 
 // function onClick(event) {
 //   event.target.disabled = true;
@@ -56,25 +113,24 @@ async function get_ai_api(context, system) {
 //   });
 // }
 
-function onChatSubmit(event) {
+const onChatSubmit = function (event) {
   event.preventDefault();
   chatSubmitButton.disabled = true;
   chatInput.disabled = true;
   chatInput.value = chatInput.value.trim();
-  globalContext.push({ role: "user", content: chatInput.value });
+  addGlobalContext({ role: "user", content: chatInput.value });
   chatInput.value = "Loading...";
   get_ai_api(globalContext, globalSystemMessage).then((response) => {
     console.log(response);
-    globalContext.push({ role: "assistant", content: response });
-    renderOutput();
+    addGlobalContext({ role: "assistant", content: response });
     chatInput.value = "";
     chatSubmitButton.disabled = false;
     chatInput.disabled = false;
     chatInput.focus();
   });
-}
+};
 
-// should render the output with alternating colors
+// renders the output with alternating colors
 function renderOutput() {
   outputDiv.innerHTML = "";
   globalContext.forEach((message) => {
