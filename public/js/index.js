948d2b70e25c0cfdc3d5299a7a62486b3bc27e41
Sahil Deshmukh <thesahildeshmukh@gmail.com>
2023-05-09 22:33:13 -0700

    Revamp storage + export/importing

--- public/js/index.js
+++ public/js/index.js
@@ -7,6 +7,8 @@ const settingsModal = new bootstrap.Modal(
 const exportText = document.getElementById("exportText");
 const exportModal = new bootstrap.Modal(document.getElementById("exportModal"));
 const personalityInput = document.getElementById("personalityInput");
+const tokenUsageSpan = document.getElementById("tokenUsageSpan");
+const contextSlider = document.getElementById("contextSlider");
 
 const noTextHTML = "<div class='h2 m-5'>Have fun!</div>";
 
@@ -22,27 +24,60 @@ if (!globalContext) {
 }
 renderOutput();
 
-const resetGlobalContext = (event = null) => {
-  if (!event) {
-    globalContext = [];
-    localStorage.setItem("globalContext", JSON.stringify(globalContext));
-    return;
+let tokenusage = parseInt(localStorage.getItem("tokenusage"));
+if (!tokenusage) {
+  tokenusage = 0;
+  localStorage.setItem("tokenusage", tokenusage);
+}
+tokenUsageSpan.innerHTML = `Token usage: ${tokenusage} (approximately ${Math.round(
+  tokenusage / 5000,
+  2
+)} cents)`;
+
+const resetSettings = (event = null) => {
+  if (event) {
+    event.preventDefault();
   }
-  event.preventDefault();
   globalContext = [];
   localStorage.setItem("globalContext", JSON.stringify(globalContext));
+  globalSystemMessage = DEFAULT_SYSTEM_MESSAGE;
+  localStorage.setItem("globalSystemMessage", globalSystemMessage);
+  tokenusage = 0;
+  localStorage.setItem("tokenusage", tokenusage);
+
+  contextLength = 3;
+
+  if (!contextLength) {
+    contextLength = 3;
+  }
+  localStorage.setItem("contextLength", contextLength);
+  contextSlider.value = contextLength;
+
+  personalityInput.value = "";
+  tokenUsageSpan.innerHTML = `Token usage: ${tokenusage} (approximately ${Math.round(
+    tokenusage / 5000,
+    2
+  )} cents)`;
+
   renderOutput();
-  console.log(Object.keys(settingsModal));
   settingsModal.hide();
 };
 
 const addGlobalContext = (context) => {
   globalContext.push(context);
-  localStorage.setItem("globalContext", JSON.stringify(globalContext));
+  if (globalContext[globalContext.length - 1].role === "assistant") {
+    localStorage.setItem("globalContext", JSON.stringify(globalContext));
+  }
   renderOutput();
 };
 
-let contextLength = 3;
+let contextLength = parseInt(localStorage.getItem("contextLength"));
+if (!contextLength && contextLength !== 0) {
+  contextLength = 3;
+  localStorage.setItem("contextLength", contextLength);
+}
+contextSlider.value = contextLength;
+
 const DEFAULT_SYSTEM_MESSAGE = "You are an affable, friendly chatbot.";
 let globalSystemMessage = localStorage.getItem("globalSystemMessage");
 if (!globalSystemMessage) {
@@ -70,25 +105,39 @@ const updateSystemMessage = (event) => {
 
 const updateContextLength = (length) => {
   contextLength = length;
+  localStorage.setItem("contextLength", contextLength);
 };
 
 const openExportModal = (event) => {
   event.preventDefault();
-  exportContext();
+  exportSettings();
   exportModal.show();
 };
 
-const exportContext = () => {
+const exportSettings = () => {
   const encryptedContext = CryptoJS.AES.encrypt(
-    JSON.stringify(globalContext),
+    JSON.stringify({
+      context: globalContext,
+      system: globalSystemMessage,
+      tokenusage: tokenusage,
+      contextLength: contextLength,
+    }),
     encryptionKey
   ).toString();
-  if (contextIsValid(globalContext)) {
+  if (
+    settingsAreValid({
+      context: globalContext,
+      system: globalSystemMessage,
+      tokenusage: tokenusage,
+      contextLength: contextLength,
+    })
+  ) {
     exportText.value = encryptedContext;
+    console.log(encryptedContext);
   }
 };
 
-const importContext = () => {
+const importSettings = () => {
   const encryptedContext = exportText.value;
   try {
     const decryptedContext = CryptoJS.AES.decrypt(
@@ -96,12 +145,21 @@ const importContext = () => {
       encryptionKey
     ).toString(CryptoJS.enc.Utf8);
     const parsedContext = JSON.parse(decryptedContext);
-    if (!contextIsValid(parsedContext)) {
+    if (!settingsAreValid(parsedContext)) {
       alert("Invalid context");
       throw new Error("Invalid context");
     }
-    globalContext = parsedContext;
+    globalContext = parsedContext.context;
     localStorage.setItem("globalContext", JSON.stringify(globalContext));
+    globalSystemMessage = parsedContext.system;
+    localStorage.setItem("globalSystemMessage", globalSystemMessage);
+    tokenusage = parseInt(parsedContext.tokenusage);
+    localStorage.setItem("tokenusage", tokenusage);
+    contextLength = parsedContext.contextLength;
+    localStorage.setItem("contextLength", contextLength);
+    contextSlider.value = contextLength;
+    personalityInput.value =
+      globalSystemMessage === DEFAULT_SYSTEM_MESSAGE ? "" : globalSystemMessage;
     renderOutput();
     exportModal.hide();
   } catch (error) {
@@ -109,16 +167,18 @@ const importContext = () => {
   }
 };
 
-const contextIsValid = (context) => {
+const settingsAreValid = (settings) => {
   try {
-    if (context.length === 0) {
-      return false;
-    }
-    if (context[context.length - 1].role !== "assistant") {
+    if (
+      settings.context.length !== 0 &&
+      settings.context[settings.context.length - 1].role !== "assistant"
+    ) {
+      console.log("last role not assistant");
       return false;
     }
     return true;
   } catch (error) {
+    console.log(error);
     return false;
   }
 };
@@ -142,33 +202,28 @@ const get_ai_api = async function (context, system) {
     }),
   })
     .then((response) => response.json())
-    .then((response) => response.output);
+    .then((response) => {
+      tokenusage += response.token_usage;
+      tokenUsageSpan.innerHTML = `Token usage: ${tokenusage} (approximately ${Math.round(
+        tokenusage / 5000,
+        2
+      )} cents)`;
+      localStorage.setItem("tokenusage", tokenusage);
+      return response.output;
+    });
 };
-
-// function onClick(event) {
-//   event.target.disabled = true;
-//   globalContext.push({ role: "user", content: userInput.value });
-//   get_ai_api(globalContext, globalSystemMessage).then((response) => {
-//     console.log(response);
-//     globalContext.push({ role: "assistant", content: response });
-//     output.innerHTML = response.replace("\n", "<br>");
-//     event.target.disabled = false;
-//   });
-// }
 
 const onChatSubmit = function (event) {
   event.preventDefault();
   chatSubmitButton.disabled = true;
-  chatInput.disabled = true;
   chatInput.value = chatInput.value.trim();
   addGlobalContext({ role: "user", content: chatInput.value });
-  chatInput.value = "Loading...";
+  chatInput.value = "";
   get_ai_api(globalContext, globalSystemMessage).then((response) => {
     console.log(response);
     addGlobalContext({ role: "assistant", content: response });
     chatInput.value = "";
     chatSubmitButton.disabled = false;
-    chatInput.disabled = false;
     chatInput.focus();
   });
 };
