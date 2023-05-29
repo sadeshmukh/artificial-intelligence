1b62676d0bbcaef84aebf25b1c04fbbd4642837f
Sahil Deshmukh <thesahildeshmukh@gmail.com>
2023-05-28 19:56:30 -0700

    Stop rich clipboard paste in index.js

--- public/js/index.js
+++ public/js/index.js
@@ -24,6 +24,45 @@ if (!globalContext) {
   localStorage.setItem("globalContext", JSON.stringify([]));
   globalContext = [];
 }
+
+function getCaretPosition(editableDiv) {
+  var caretPos = 0,
+    sel,
+    range;
+  if (window.getSelection) {
+    sel = window.getSelection();
+    if (sel.rangeCount) {
+      range = sel.getRangeAt(0);
+      if (range.commonAncestorContainer.parentNode == editableDiv) {
+        caretPos = range.endOffset;
+      }
+    }
+  } else if (document.selection && document.selection.createRange) {
+    range = document.selection.createRange();
+    if (range.parentElement() == editableDiv) {
+      var tempEl = document.createElement("span");
+      editableDiv.insertBefore(tempEl, editableDiv.firstChild);
+      var tempRange = range.duplicate();
+      tempRange.moveToElementText(tempEl);
+      tempRange.setEndPoint("EndToEnd", range);
+      caretPos = tempRange.text.length;
+    }
+  }
+  return caretPos;
+}
+
+var ce = document.querySelector("[contenteditable]");
+ce.addEventListener("paste", function (e) {
+  e.preventDefault();
+  var text = e.clipboardData.getData("text/plain");
+  let caretPosition = getCaretPosition(e.target);
+  e.target.innerText =
+    e.target.innerText.substring(0, caretPosition) +
+    text +
+    e.target.innerText.substring(caretPosition);
+  // TODO: Fix caret position
+});
+
 renderOutput();
 
 let tokenusage = parseInt(localStorage.getItem("tokenusage"));
